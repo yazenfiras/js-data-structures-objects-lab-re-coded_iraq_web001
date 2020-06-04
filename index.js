595f6838c041d27d3ed402b6b18a68fc446f9402
yazen firas <psyazen3@gmail.com>
2020-06-04 10:18:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Write your solution in this file!
 
-function nondestructivelyUpdateObject(obj, key, value) {
+let 
+
+function updateDriverWithKeyAndValue(obj, key, value) {
   return Object.assign({}, obj, { [key]: value });
 }
