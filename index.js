1d918faaa1d74f1d54b457cacdcb6269dde781f8
yazen firas <psyazen3@gmail.com>
2020-06-04 10:19:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver, { [key]: value });
 }
 
-function destructivelyUpdateObject (obj, key, value) {
+function destructivelyUpdateDriverWithKeyAndValue (obj, key, value) {
   obj[key] = value;
  
   return obj;
